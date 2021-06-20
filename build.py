c575e05331d2cfb860552185744c19cbb5ff805e
gsanm <g.sanmartin.silva@gmail.com>
2021-06-20 18:31:24 -0400

    functional page

--- build.py
+++ build.py
@@ -3,16 +3,17 @@ import jinja2
 import yaml
 
 render_vars = {
-    "greeting": "Oh hi Mark"
+    "website_name": "JOOOOLOOO"
 }
 
 params = yaml.load(open("info.yaml"), Loader=yaml.FullLoader)
 
-
+basic_info = params['basic_info']
+publication_list = params['publication_list']
+print(publication_list)
 environment = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
-output_text = environment.get_template('index.html').render(render_vars)
+output_text = environment.get_template('index.html').render(basic_info, publication_list=publication_list)
 
-print(output_text)
 
 
 with open(os.path.join('rendered_files', 'index.html'), "w") as result_file:

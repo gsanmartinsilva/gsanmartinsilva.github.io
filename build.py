59087eaaf209f380356cfc9ea175d0347a5e40d6
gsanm <g.sanmartin.silva@gmail.com>
2021-06-20 19:03:24 -0400

    ready for use

--- build.py
+++ build.py
@@ -2,19 +2,15 @@ import os
 import jinja2
 import yaml
 
-render_vars = {
-    "website_name": "JOOOOLOOO"
-}
-
 params = yaml.load(open("info.yaml"), Loader=yaml.FullLoader)
-
 basic_info = params['basic_info']
 publication_list = params['publication_list']
-print(publication_list)
+social = params['social']
+
 environment = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
-output_text = environment.get_template('index.html').render(basic_info, publication_list=publication_list)
-
-
+output_text = environment.get_template('index.html').render(basic_info=basic_info,
+                                                            publication_list=publication_list,
+                                                            social=social)
 
 with open(os.path.join('rendered_files', 'index.html'), "w") as result_file:
     result_file.write(output_text)
